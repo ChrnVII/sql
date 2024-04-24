a9341790b3a5fe2b1d485ec8e6a299c9409a3af6
suyunjun <3495435816@qq.com>
2024-04-24 15:40:39 +0800

    On branch master
    Initial commit
    
    Changes to be committed:
    	new file:   demo/pom.xml
    	new file:   demo/src/main/java/com/example/Main.java
    
    Untracked files:
    	demo/target/
    	test.code-workspace

---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/study", "root", "123456");
+                Statement stat = connection.createStatement();) {
+            ResultSet set = stat.executeQuery("select * from student;");
+            
+            while (set.next()) {
+                int id = set.getInt("sid");
+                String name = set.getString("name");
+                String gender = set.getString("gender");
+                System.out.println(id + "\t" + name + "\t" + gender);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
